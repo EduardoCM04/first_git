5e2c606aceac955617de7278d35f7278308a55cf
Eduardo Comas Montaño <eduardocm0408@gmail.com>
2022-06-26 12:12:04 -0400

    Recordando programacion orientada a objetos

--- hola_mundo.py
+++ hola_mundo.py
@@ -1,3 +1,13 @@
 
-a = "Hola mundo"
-print(a)
+from unicodedata import name
+
+
+class animales():
+    def __init__(self, name):
+        self.name = name
+    def test(serl):
+        print("Esta es una prueba")
+
+perro = animales("Doki")
+print(perro.name)
+perro.test()
